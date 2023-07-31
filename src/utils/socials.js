087335a0472e17ea9c6e45209154ed01f09fd514
Ryan Frawley <me@ryanfrawley.io>
2023-07-31 18:20:51 -0400

    fix: instagram url

--- src/utils/socials.js
+++ src/utils/socials.js
@@ -1,4 +1,4 @@
 export const TWITTER = "https://twitter.com/gh0st_turtle";
 export const LINKEDIN = "https://linkedin.com/in/ryan-frawley";
-export const INSTAGRAM = "https://instagram.com/flywithry";
+export const INSTAGRAM = "https://instagram.com/flywithrye";
 export const GITHUB = "https://github.com/ryanfrawley";
